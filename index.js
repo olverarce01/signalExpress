61de13e8ff1712c27c3e4fb561539798ee6b77b1
Piero <piero.suarez.zuniga@alumnos.uta.cl>
2023-06-04 16:11:06 -0400

    u

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ app.enable('trust proxy');
 // Configurar CORS
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const port = process.env.PORT || 3001;
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
 const peerServer = ExpressPeerServer(server, {
+    port: port,
     debug: true,
-    path: '/myapp'
+    path: '/myapp',
+    key: 'peerjs',
 });
 
 app.use('/peerjs', peerServer);
 
-//on conect 
-peerServer.on('connection', (client) => {
-    console.log('Client connected', client.id);
-});
+// //on conect 
+// peerServer.on('connection', (client) => {
+//     console.log('Client connected', client.id);
+// });
 
-//on disconnect
-peerServer.on('disconnect', (client) => {
-    console.log('Client disconnected', client.id);
-} );
+// //on disconnect
+// peerServer.on('disconnect', (client) => {
+//     console.log('Client disconnected', client.id);
+// } );
